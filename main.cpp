98a1656f388d35efa066a128a9f8242f400d81b4
Piekarz20 <102181834+Piekarz20@users.noreply.github.com>
2024-10-01 14:18:10 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,22 @@ using namespace std;
 FILE *plik;
 int liczby[100];
 int ile,i,j,maks;
+//--------------
+//--------------
+
+void sortowanie(){
+    int pomoc;
+for (i=0;i<ile-1;i++){
+    for (j=0;j<ile-1;j++){
+        if (liczby[j]>liczby[j+1]){
+            pomoc = liczby[j];
+            liczby[j]=liczby[j+1];
+            liczby[j+1]=pomoc;
+        }
+    }
+}
+
+}
 
 int main()
 {
@@ -26,7 +42,11 @@ int main()
         }
         fclose(plik);
         cout<<"odczytano: "<<ile<<" elementow"<<endl;
-        cout<<"wartosc maksymalna to: "<<maks;
+        cout<<"wartosc maksymalna to: \n"<<maks<<endl;
+        sortowanie();
+        for (i=0;i<ile;i++){
+            cout<<liczby[i]<<", ";
+        }
     } else
     {
         cout<<"error file";
